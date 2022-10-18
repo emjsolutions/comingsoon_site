6c8b9b5ced87bdc444af1794caef12676966e46a
Mark Scerri <m.scerri@gmail.com>
2022-10-18 22:43:58 +0200

    Update packages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from "../components/layout";
 import Timer from "../components/timer"
-import Particles from 'react-particles-js';
+import Particles from 'react-tsparticles';
 
 const particlesOptions = {
   particles: {
